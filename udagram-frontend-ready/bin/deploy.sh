e34e0da0c88a8479528b4bd4abe7b5c9b108820c
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-30 09:48:45 -0600

    cmabio de dist a www

--- udagram-frontend-ready/bin/deploy.sh
+++ udagram-frontend-ready/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./dist s3://frontend-s3-hosting-a-full-stack-application/
+aws s3 cp --recursive --acl public-read ./www s3://frontend-s3-hosting-a-full-stack-application/
