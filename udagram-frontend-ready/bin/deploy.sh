72738be846b2b147e04289cc8125e4c401145e68
Dieg Luna <diegofcolunalopez@gmail.com>
2022-01-30 12:05:02 -0600

    ng build and -> aws s3 cp

--- udagram-frontend-ready/bin/deploy.sh
+++ udagram-frontend-ready/bin/deploy.sh
@@ -1,2 +1,2 @@
-# aws s3 cp --recursive --acl public-read ./www s3://frontend-s3-hosting-a-full-stack-application/
 ng build
+aws s3 cp --recursive --acl public-read ./www s3://frontend-s3-hosting-a-full-stack-application/
